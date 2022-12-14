6de6d98f8542587672756a6384ba8bfa27dcf35f
riaagita07 <riaagit0707@gmail.com>
2022-12-14 09:49:06 +0700

    first commit agita

--- App/Pages/TaskScreen/index.js
+++ App/Pages/TaskScreen/index.js
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
+import * as React from "react";
+import { View, Text } from "react-native";
 
 export default TaskScreen = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Task Screen</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Task Screen Agita</Text>
     </View>
   );
 };
